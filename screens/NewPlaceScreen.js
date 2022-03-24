b1f62b0c1bfc815688ae8434a30706dd5c73367d
studioMac-189 <studiomac-189@studioMac-189s-iMac.local>
2022-03-24 16:42:05 -0700

    first commit

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  TextInput, 
+  Button, 
+  StyleSheet, 
+  ScrollView 
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as PlacesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
+
+const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState('');
+  const [selectedImage, setSelectedImage] = useState();
+
+  const dispatch = useDispatch();
+
+  const titleChangeHandler = text => {
+    // you could add validation
+    setTitleValue(text);
+  };
+
+  const imageTakenHandler = imagePath => {
+    setSelectedImage(imagePath);
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(PlacesActions.addPlace(titleValue, selectedImage));
+    props.navigation.goBack();
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput 
+          style={styles.textInput} 
+          onChangeText={titleChangeHandler}
+          value={titleValue}  
+        />
+        <ImagePicker onImageTaken={imageTakenHandler} />
+        <Button 
+          title='Save place'  
+          color={Colors.primary} 
+          onPress={savePlaceHandler}  
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: 'Add Place'
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
+
+export default NewPlaceScreen;
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlacesDetailScreen = props => {
+  return (
+    <View>
+      <Text>PlacesDetailScreen</Text>
+    </View>
+  );
+};
+
+PlacesDetailScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('placeTitle')
+  };
+};
+
+const styles = StyleSheet.create({}); 
+
+export default PlacesDetailScreen;
